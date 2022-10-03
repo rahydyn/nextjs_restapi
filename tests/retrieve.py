dbeab8a19923f0e33c5b0b46f1ea62927fcfb688
rahydyn <kanaokasatoru@gmail.com>
2022-10-04 05:16:34 +0900

    modified

--- tests/retrieve.py
+++ tests/retrieve.py
@@ -10,10 +10,10 @@ def retrieve_response(form_id):
     SCOPES = "https://www.googleapis.com/auth/forms.responses.readonly"
     DISCOVERY_DOC = "https://forms.googleapis.com/$discovery/rest?version=v1"
 
-    store = file.Storage('token.json')
+    store = file.Storage('./token.json')
     creds = None
     if not creds or creds.invalid:
-        flow = client.flow_from_clientsecrets('client_secrets.json', SCOPES)
+        flow = client.flow_from_clientsecrets('./client_secrets.json', SCOPES)
         creds = tools.run_flow(flow, store)
     service = discovery.build('forms', 'v1', http=creds.authorize(
         Http()), discoveryServiceUrl=DISCOVERY_DOC, static_discovery=False)
